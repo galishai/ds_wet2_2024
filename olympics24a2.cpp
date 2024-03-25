edc4da89bc9c1fbeeb1a899d1276734a4b343c3a
Gal Ishai <74329671+galishai@users.noreply.github.com>
2024-03-25 16:58:33 +0200

    Add files via upload

--- olympics24a2.cpp
+++ olympics24a2.cpp
@@ -63,6 +63,7 @@ StatusType olympics_t::remove_team(int teamId)
         TeamByPower teamPowerFinder(teamId, team->m_info->m_wins, team->m_info->m_power);
         m_teamsByID->removeNode(&teamIDFinder);
         m_teamsByPower->removeNode(&teamPowerFinder);
+        m_teamsByPower->updateMaxRec(maxNode(m_teamsByPower->m_root));
     }
     if(m_teamsHash->m_occupancy == 0)
     {
