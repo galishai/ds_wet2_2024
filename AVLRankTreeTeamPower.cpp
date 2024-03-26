1187e9513feae102bd8bc1eec838b02496ba9f93
Gal Ishai <74329671+galishai@users.noreply.github.com>
2024-03-27 01:19:39 +0200

    Add files via upload

--- AVLRankTreeTeamPower.cpp
+++ AVLRankTreeTeamPower.cpp
@@ -266,8 +266,8 @@ Node<TeamByPower> *AVLRankTreePower::RightRightRotation(Node<TeamByPower> *nodeB
     //nodeB->m_maxRank = max(getMax(nodeB->m_left), getMax(nodeB->m_right));
     //nodeA->m_maxRank = max(getMax(nodeA->m_left), getMax(nodeA->m_right));
     nodeA->m_maxRank = nodeB->m_maxRank;
-    updateMax(nodeB);
-    updateMax(nodeA);
+    updateMaxRec(nodeB);
+    updateMaxRec(nodeA);
     return nodeA;
 
 }
@@ -321,8 +321,8 @@ Node<TeamByPower> *AVLRankTreePower::LeftLeftRotation(Node<TeamByPower> *nodeB)
 
     //nodeB->m_maxRank = max(getMax(nodeB->m_left), getMax(nodeB->m_right));
     nodeA->m_maxRank = nodeB->m_maxRank;
-    updateMax(nodeB);
-    updateMax(nodeA);
+    updateMaxRec(nodeB);
+    updateMaxRec(nodeA);
     //nodeA->m_maxRank = max(getMax(nodeA->m_left), getMax(nodeA->m_right));
     return nodeA;
 }
