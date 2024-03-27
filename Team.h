aab5674a04126e2dd3d65952e133e31c441209a9
Gal Ishai <74329671+galishai@users.noreply.github.com>
2024-03-27 14:10:24 +0200

    Add files via upload

--- Team.h
+++ Team.h
@@ -30,6 +30,15 @@ public:
         m_medianPlayer = nullptr;
     }
 
+    explicit Team(Team *other)
+    {
+        m_teamID = other->m_teamID;
+        m_wins = other->m_wins;
+        m_power = other->m_power;
+        m_playersByCreated = nullptr;
+        m_playersByStrength = nullptr;
+    }
+
     virtual bool operator==(Team* other) const = 0;
     virtual bool operator<(Team* other) const = 0;
 
--- TeamByID.h
+++ TeamByID.h
@@ -22,6 +22,17 @@ public:
         delete m_playersByStrength;
     }
 
+    explicit TeamByID(TeamByID *other): Team(other)
+    {
+        m_teamID = other->m_teamID;
+        m_wins = other->m_wins;
+        m_power = other->m_power;
+        m_playersByCreated = nullptr;
+        m_playersByStrength = nullptr;
+    }
+
+
+
     bool operator==(Team* other) const override
     {
         if(other == nullptr)
