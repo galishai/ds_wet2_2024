dfde48d8b5ec234284446382a6f47ef22521c179
Gal Ishai <74329671+galishai@users.noreply.github.com>
2024-03-28 11:02:19 +0200

    Add files via upload

--- script_v1.sh
+++ script_v1.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Loop over all files in tests/logs_in ending in ,in
-for file in tests/logs_in/*.in; do
+for file in lotan_tests/logs_in/*.in; do
     # Get the base name of the file
     base_name=$(basename "$file" .in)
     
     # Run the ./main program with the input file and redirect the output to tests/logs_myout
     echo "now doing ${file}"
-    ./a.out < "$file" > "tests/logs_myout/${base_name}.out"
+    ./a.out < "$file" > "lotan_tests/logs_myout/${base_name}.out"
     # Diff the output file with the expected output file, ignoring space changes
-    diff_output=$(diff -w "tests/logs_myout/${base_name}.out" "tests/logs_out/${base_name}.out")
+    diff_output=$(diff -w "lotan_tests/logs_myout/${base_name}.out" "lotan_tests/logs_out/${base_name}.out")
     if [ $? -ne 0 ]; then
         echo "${file} fail"
     fi
